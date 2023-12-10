cab0ce184910393072df73c392b985c80225a584
Theo Gicquel <theo.gicquel.work@gmail.com>
2023-12-10 20:01:38 +0100

    rework hero section layout

--- 1_integration/src/lib/Hero.svelte
+++ 1_integration/src/lib/Hero.svelte
@@ -1,15 +1,36 @@
 <script>
 	import Heading from "./Heading.svelte";
+	import Navbar from "./Navbar.svelte";
 
 </script>
 
-<div class="bg-hero-img bg-cover h-screen bg-no-repeat ">
-    <div class="grid grid-cols-5 grid-rows-3 gap-0 px-60 py-36">
+<div class="bg-hero-img bg-cover h-screen bg-no-repeat  ">
+    <div class="flex flex-col justify-between   h-full px-60 py-36  ">
       
-      <Heading title="Sustameekt" subtitle="Exploration" />
+      <!-- Main header -->
+      <div class="grid grid-cols-5 grid-rows-1 animate-[wiggle_8s_ease-in-out_infinite]">
+        <Heading title="Sustameekt" subtitle="Exploration" />
+      </div>
+
+      <div class="flex flex-col justify-end  p-5 animate-[wiggle_7s_ease-in-out_infinite]">
+
       
+      <div class="col-start-2 col-end-5 px-20 w-2/3 mx-auto ">
+        <Navbar />
+      </div>
+
+      <!-- Red separator -->
+      <div class="col-span-5 text-white  ">
+        <div class="flex-grow  border-red-800 border-t-2 "></div>
+
+      </div>
+
+      </div>
+
+
+
+
     </div>
 </div>
 
   
-
